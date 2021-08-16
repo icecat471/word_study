e4434b3c8cb32be2373ebb05a2f7b0719283ef57
icecat471@gmail.com <icecat471@gmail.com>
2021-08-17 06:27:22 +0900

    Change bootstrap to antd

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,28 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
-import Header from './components/header/Header';
+
+import { Layout } from 'antd';
+
+import Nav from './components/header/Nav';
+
 import Home from './components/home/Home';
 import WordList from './components/wordList/WordList';
 import Memorize from './components/memorize/Memorize';
 import Test from './components/test/Test';
 
+const { Footer } = Layout;
+
 const App: React.FC = () => {
   return (
     <BrowserRouter>
-      <Header></Header>
-      <div>
-        <Route exact path='/word_study' component={Home}/>
-        <Route path='/word_study/list' component={WordList}/>
-        <Route path='/word_study/memorize' component={Memorize}/>
-        <Route path='/word_study/test' component={Test}/>
-      </div>
+      <Layout className="layout">
+        <Nav></Nav>
+        <Route exact path="/" component={Home}></Route>
+        <Route exact path="/list" component={WordList}></Route>
+        <Route exact path="/memorize" component={Memorize}></Route>
+        <Route exact path="/test" component={Test}></Route>
+        <Footer style={{ textAlign: 'center' }}>Study Korean for Mizu.</Footer>
+      </Layout>
     </BrowserRouter>
   );
 }
